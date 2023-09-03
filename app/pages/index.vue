25ad42816abeb7caff94f48d42a45f04501117e8
Peyton Harris <95325343+peyhar@users.noreply.github.com>
2023-09-03 16:53:50 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,13 +4,11 @@
       <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
         <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
 
-             </div>
       </div>
+
     </div>
   </section>
 </template>
-
-
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator';
